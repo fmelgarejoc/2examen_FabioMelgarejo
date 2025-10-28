8a94c1282079d8a55c9a3e255112bb2e3941fc62
Fabio <eitnar.fabio@gmail.com>
2025-10-27 23:06:04 -0400

    9. feat(web): crear servicio empleados y consumir API

--- frontend/src/app/empleados/empleados.ts
+++ frontend/src/app/empleados/empleados.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleadoservice } from '../core/services/empleadoservice';
 
 @Component({
   selector: 'app-empleados',
@@ -7,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './empleados.scss',
 })
 export class Empleados {
+
+  empleados: any[] = [];
+  constructor(private empleaadoService:Empleadoservice){
+    this.listarEmpleados();
+  }
+
+  listarEmpleados():void{
+    this.empleaadoService.listarEmpleados().subscribe({
+      next:(data)=>{
+        this.empleados=data
+        console.log(this.empleados);
+      },
+      error:(err)=>console.error('Error al cargar',err)
+      
+    })
+  }
 
 }
