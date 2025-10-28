2a9298d3a1edf985fe926a5f47587b3a207d225c
Fabio <eitnar.fabio@gmail.com>
2025-10-27 21:07:23 -0400

    3. feat(api): crear modelo Empleado

---
+++ backend/app/Models/Empleado.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Empleado extends Model
+{
+
+    protected $table = 'empleados';
+
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'correo',
+        'salario'
+    ];
+
+    protected $casts = [
+        'salario' => 'decimal:2'
+    ];
+}
